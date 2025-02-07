036ca98a45629c21ae209481f7c3a193b7eed0e3
Henrik Ackler <henrikackler@u.boisestate.edu>
2025-02-07 11:12:43 -0700

    basic CLI + error handling

--- ASCIIArtController.java
+++ ASCIIArtController.java
@@ -3,41 +3,45 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**Driver class for generating ASCII art.
+/**
+ * Driver class for generating ASCII art.
  *
  * @author Henrik Ackler
  */
-public class ASCIIArtController
-{
-   public static void main(String[] args)
-   {
-      CharMapper myMapper = new CharMapper(new Font("Arial", Font.PLAIN, 10), 13);
-      //myMapper.makeImgSet();
-      myMapper.makeValueMaps();
-      
-      long timerStart = System.currentTimeMillis();
-      
-      ImageReader myReader = new ImageReader("cat.png");
-      //myReader.makeOutput("catLuminosity");
-      myReader.readImage();
+public class ASCIIArtController {
+   public static void main(String[] args) {
+      if (args.length != 2) {
+         System.out.println("\nImproper usage. Format: $ java ASCIIArtController [recalculate] [image]\n");
+         System.out.println("\tRecalculate: \t0 to use existing ASCII calculations, 1 to recalculate");
+         System.out.println("\tImage: \t\tName of image file located within ./images");
+         System.out.println("\tExample: \t$ java ASCIIArtController 0 cat.png");
+      } else {
+         try {
+            CharMapper myMapper = new CharMapper(new Font("Arial", Font.PLAIN, 10), 13);
+            if(Integer.parseInt(args[0]) == 1)
+            {
+               myMapper.makeImgSet();
+            }
+            myMapper.makeValueMaps();
 
-      asciiGenerator myGenerator = new asciiGenerator(myReader.getLumArray(), myMapper);
-      myGenerator.getAsciiArt();
-      
-      long timerLap1 = System.currentTimeMillis();      
+            ImageReader myReader = new ImageReader("./images/" + args[1]);
+            myReader.readImage();
 
-      try {
-         BufferedWriter artOut = new BufferedWriter(new FileWriter("output.txt"));
-         artOut.write(myGenerator.getAsciiArtString());
-         artOut.close();
-      } catch (IOException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
+            asciiGenerator myGenerator = new asciiGenerator(myReader.getLumArray(), myMapper);
+            myGenerator.getAsciiArt();
+
+            BufferedWriter artOut = new BufferedWriter(new FileWriter("output.txt"));
+            artOut.write(myGenerator.getAsciiArtString());
+            artOut.close();
+            System.out.println("Generation complete.");
+         } catch (IOException e)
+         {
+            System.out.println(e.toString() + "\nPlease check input file, or recalculate ASCII");
+         } catch (NumberFormatException e)
+         {
+            System.out.println(e.toString() + "\nPlease use a [recalculate] value of 0 or 1");
+         }
       }
 
-      long timerFinish = System.currentTimeMillis();
-
-      System.out.println("The program took " + ( (timerLap1 - timerStart)) + "ms to generate the frame.");
-      System.out.println("The program took " + ( (timerFinish - timerStart)) + "ms to generate and export the frame.");
    }
 }
